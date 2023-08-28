cbf3b258410dcb55eb77f29e8d8f95ddb5359bbc
Adilakshmi <lakshmigunapati77@gmail.com>
2023-08-28 17:52:47 -0400

    added text in repository

--- src/main/java/com/ownproject/repository/BooksRepository.java
+++ src/main/java/com/ownproject/repository/BooksRepository.java
@@ -1,7 +1,7 @@
 package com.ownproject.repository;
 
 import org.springframework.data.repository.CrudRepository;
-//repository that extends CrudRepository
+//repository that extends CrudRepository added text
 import com.ownproject.model.Books;
 
 public interface BooksRepository extends CrudRepository<Books, Integer> {
